cc5c1dc2832b8ff65389dc861319f2fb67a16f76
Luis Zuniga <lezv.dc@gmail.com>
2020-12-18 07:57:24 -0600

    CSS login

--- linear-algebra-solver/src/componentes/Formulario-login.js
+++ linear-algebra-solver/src/componentes/Formulario-login.js
@@ -1,11 +1,35 @@
 import React from 'react'
-import {Container} from 'react-bootstrap'
+import {Container, Form, Card, Button} from 'react-bootstrap'
 
 const Formulario_login = () => {
 
     return(
-        <Container>
-
+        <Container fluid className="container_login">
+            <div className="tittle_login">
+                <h1>Bienvenido a Linear Algebra Solver</h1>
+                <h5>Por favor, inicie sesión en su cuenta</h5>
+            </div>
+            <Container className="container_login_card">
+                <Card></Card>
+                <Card>
+                    <h1 className="title">Login</h1>
+                    <Form>
+                        <Form.Group>
+                            <Form.Control id="user" type="text" required="required" />
+                            <Form.Label>Usuario</Form.Label>
+                            <div className="bar"></div>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Control id="pass" type="password" required />
+                            <Form.Label>Password</Form.Label>
+                            <div className="bar"></div>
+                        </Form.Group>
+                        <Form.Group className="submit-button">
+                            <Button><span>Enviar</span></Button>
+                        </Form.Group>
+                    </Form>
+                </Card>
+            </Container>
         </Container>
     )
 
---
+++ linear-algebra-solver/src/pages/Login.js
@@ -0,0 +1,17 @@
+import '../css/login.css'
+import React from 'react'
+import Log from '../componentes/Formulario-login'
+
+class Login extends React.Component{
+
+    render(){
+
+        return(
+            <Log/>
+        )
+
+    }
+
+}
+
+export default Login;
