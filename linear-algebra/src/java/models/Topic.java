8bba5c27efb7a4dae4c83c5f5e79fa70bc921a07
Abdiel Reyes <abykings1@hotmail.com>
2021-01-16 16:49:06 -0600

    added get only topics

--- linear-algebra/src/java/models/Topic.java
+++ linear-algebra/src/java/models/Topic.java
@@ -27,24 +27,56 @@ public class Topic {
         SQL = new DBConnection();
         con = SQL.conectar();
     }
-    public JSONObject getTopics(){
+
+    public JSONObject getOnlyTopics() {
+        JSONObject data = new JSONObject();
+        try {
+
+            String query = "select * from tema";
+            PreparedStatement ps = con.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            JSONArray arr = new JSONArray();
+            if(rs.next()){
+                do{
+                    JSONObject t = new JSONObject();
+                t.put("id_tema", rs.getString("id_tema"));
+                t.put("titulo", rs.getString("titulo"));
+                t.put("video", rs.getString("video"));
+                arr.put(t);
+                }while(rs.next());
+                data.put("state", 200);
+                data.put("temas", arr);
+                data.put("message", "Temas  encontrados");
+            }else{
+                 data.put("state", 404);
+                data.put("message", "No hay temas");
+            }
+            
+        
+        } catch (SQLException ex) {
+            Logger.getLogger(Topic.class.getName()).log(Level.SEVERE, null, ex);
+             data.put("state", 500);
+                data.put("message", "Error al sacar temas");
+        }
+        return data;
+    }
+
+    public JSONObject getTopics() {
         JSONObject data = new JSONObject();
         String query = "SELECT tema.titulo as titulo, tema.video as video_tema, subtema.titulo as subtitulo, subtema.descripcion as descripcion, tema.id_tema as id_tema,"
                 + " subtema.id_subtema as id_subtema FROM tema INNER JOIN subtema on subtema.id_tema = tema.id_tema; ";
         PreparedStatement ps;
         try {
             ps = con.prepareStatement(query);
-            
+
             ResultSet rs = ps.executeQuery();
-            
-            if(rs.next()){
-               
-                
-                
+
+            if (rs.next()) {
+
                 JSONArray arr = new JSONArray();
-               do{
-                   JSONObject topic = new JSONObject();
-                   
+                do {
+                    JSONObject topic = new JSONObject();
+
                     topic.put("tema", rs.getString("titulo"));
                     topic.put("id_tema", rs.getString("id_tema"));
                     topic.put("video_tema", rs.getString("video_tema"));
@@ -52,21 +84,21 @@ public class Topic {
                     topic.put("id_subtema", rs.getString("id_subtema"));
                     topic.put("descripcion", rs.getString("descripcion"));
                     arr.put(topic);
-                }while(rs.next());
-              
+                } while (rs.next());
+
                 data.put("topics", arr);
-                data.put("state",200);
-                data.put("message","Temas y subtemas encontrados");
-            }else{
-                data.put("state",200);
-                data.put("message","No hay temas");
+                data.put("state", 200);
+                data.put("message", "Temas y subtemas encontrados");
+            } else {
+                data.put("state", 200);
+                data.put("message", "No hay temas");
             }
         } catch (SQLException ex) {
-            data.put("state",200);
-            data.put("message","Error al obtener temas");
+            data.put("state", 200);
+            data.put("message", "Error al obtener temas");
             Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return data;
     }
 }
